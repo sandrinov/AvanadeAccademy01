ef945ccf8ec023db0a05f75d5a1d833ef7692e13
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2019-02-05 10:41:33 +0100

    API fixed

---
+++ AvanadeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AvanadeAPI.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private AvanadeRepository _repository;
+
+        public EmployeesController()
+        {
+            _repository = new AvanadeRepository();
+        }
+        [Route("api/getemployees", Name = "GetAllEmployees")]
+        public List<Employee> GetAllEmployees()
+        {
+            return _repository.GetAllEmployees();
+        }
+    }
+}
